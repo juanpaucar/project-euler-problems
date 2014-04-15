4bddd3ec9ecf5af163d947a9e247c11704080b8e
juanpaucar <juan.paucar@est.epn.edu.ec>
2014-04-14 22:25:35 -0500

    Used time added

--- problem1/main.py
+++ problem1/main.py
@@ -2,12 +2,18 @@
 #The sum of these multiples is 23.
 #Find the sum of all the multiples of 3 or 5 below 1000."""
 
+import time
 __author__ = 'Juan Paucar'
 
 sum= 0
+
+start = time.clock()
 
 for i in range(3,1000):
     if (i%3==0 or i%5==0):
         sum+=i
 else:
     print sum
+
+stop = time.clock()
+print "Elapsed time: %s ms" %(str((stop-start)*1000))
